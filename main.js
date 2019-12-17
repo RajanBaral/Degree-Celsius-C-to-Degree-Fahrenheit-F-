7b56f7ff7ee54c09f7e382cdc1259c6d576375ff
Rajan Baral <razunbaral@gmail.com>
2019-12-17 12:59:59 -0500

    This is the new updated javaScript file

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-function dTof(){
-let degree = document.getElementById('d').value;
-  let g = document.getElementById('fer').value = (degree * 9/5) + 32 ;
-  document.getElementById('demo').innerHTML = g;
-}
-
-c.onclick = function(){
- dTof();
-}
-
-
+function degreeTofahrenheit(){
+  let degree = document.getElementById('Celsius').value;
+    let g = document.getElementById('fahrenheit').value = (degree * 9/5) + 32 ;
+    document.getElementById('demo').innerHTML = degree + " degree celsius is equal to " + g + " degree Fahrenheit.";
+  }
+  
+  degreeToFahrenheit.onclick = function(){
+    degreeTofahrenheit();
+  }
+  
+  
+  
