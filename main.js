d50fd4c9c1b0ad8fb59bf2f5a66e24d279a7b4d3
Rajan Baral <razunbaral@gmail.com>
2019-12-17 13:42:32 -0500

    made changes on main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function degreeTofahrenheit(){
   let degree = document.getElementById('Celsius').value;
-    let g = document.getElementById('fahrenheit').value = (degree * 9/5) + 32 ;
-    document.getElementById('demo').innerHTML = degree + " degree celsius is equal to " + g + " degree Fahrenheit.";
+    let Fahrenheit = document.getElementById('fahrenheit').value = (degree * 9/5) + 32 ;
+    document.getElementById('demo').innerHTML = degree + " degree celsius is equal to " + Fahrenheit + " degree Fahrenheit.";
   }
   
   degreeToFahrenheit.onclick = function(){
